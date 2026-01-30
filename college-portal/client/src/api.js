7720fafb95c35fcf20375966e768333f1a7be816
YeduguruHemaSriReddy <175817381+YeduguruHemaSriReddy@users.noreply.github.com>
2026-01-31 00:19:34 +0530

    Frontend connected to live backend

--- college-portal/client/src/api.js
+++ college-portal/client/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Point all API calls to your deployed backend on Render
 const API = axios.create({
-  baseURL: "https://srit-7gpl.onrender.com/api", // your Render backend URL with /api
+  baseURL: "https://srit-backend-opdk.onrender.com/api", // your Render backend URL with /api
 });
 
 export default API;
